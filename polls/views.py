89e75d29e00561b826450cde8e530151df177563
milee yu <mileeyu@gmail.com>
2020-06-16 02:43:42 -0700

    added candidate queryset

--- polls/views.py
+++ polls/views.py
@@ -6,9 +6,11 @@ from .models import Candidate, Question, Response
 def index(request):
     response_list = Response.objects.all()
     question_list = Question.objects.all().order_by('number')
+    candidate_list = Candidate.objects.all()
     template_name = 'polls/index.html'
     context = {
         'response_list': response_list,
         'question_list': question_list,
+        'candidate_list': candidate_list,
     }
     return render(request, template_name, context)
